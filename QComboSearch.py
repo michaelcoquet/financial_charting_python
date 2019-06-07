6915a56df9b8e624801fbe3aac179d756738c18d
mike <m.coquet2@gmail.com>
2019-06-07 16:33:35 -0600

    adds a database of all possible currency pairs and extends QComboBox to make a search box for symbols

--- QComboSearch.py
+++ QComboSearch.py
@@ -5,9 +5,12 @@ from PyQt5.QtGui import QFont
 from PyQt5 import QtCore
 from PyQt5.QtGui import QKeyEvent
 from PyQt5.QtWidgets import QCompleter, QComboBox
-import csv
+import sqlite3
+from sqlite3 import Error
+
 
 class QComboSearch(QComboBox):
+
     def __init__(self, parent=None):
         super(QComboSearch, self).__init__(parent)
 
@@ -18,48 +21,77 @@ class QComboSearch(QComboBox):
         self.view().setFont(font)
         self.setEditable(True)
 
-        # add a filter model to filter matching items
+        #self.load_data('')
+        # # add a filter model to filter matching items
         self.pFilterModel = QSortFilterProxyModel(self)
-        self.pFilterModel.setFilterCaseSensitivity(Qt.CaseInsensitive)
-        self.pFilterModel.setSourceModel(self.model())
-
-        # add a completer, which uses the filter model
+        # self.pFilterModel.setFilterCaseSensitivity(Qt.CaseInsensitive)
+        # self.pFilterModel.setSourceModel(self.model())
+        #
+        # # add a completer, which uses the filter model
         self.completer = QCompleter(self.pFilterModel, self)
-        # always show all (filtered) completions
-        self.completer.setCompletionMode(QCompleter.UnfilteredPopupCompletion)
-        self.setCompleter(self.completer)
-
-        # connect signals
-        self.lineEdit().textEdited.connect(self.pFilterModel.setFilterFixedString)
+        # # always show all (filtered) completions
+        # self.completer.setCompletionMode(QCompleter.UnfilteredPopupCompletion)
+        # self.setCompleter(self.completer)
+        #
+        # # connect signals
+        # self.lineEdit().textEdited.connect(self.pFilterModel.setFilterFixedString)
         self.completer.activated.connect(self.on_completer_activated)
+
+        self.lineEdit().textEdited.connect(self.load_data)
 
         # check if line edit has ever been changed
         self.textChanged = False
+        # self.data_loaded = False
+    #
+    # # on selection of an item from the completer, select the corresponding item from combobox
 
-    # on selection of an item from the completer, select the corresponding item from combobox 
     def on_completer_activated(self, text):
         if text:
             index = self.findText(text)
             self.setCurrentIndex(index)
             self.activated[str].emit(self.itemText(index))
 
-    # on model change, update the models of the filter and completer as well 
-    def setModel(self, model):
-        super(QComboSearch, self).setModel(model)
-        self.pFilterModel.setSourceModel(model)
-        self.completer.setModel(self.pFilterModel)
-
-    # on model column change, update the model column of the filter and completer as well
-    def setModelColumn(self, column):
-        self.completer.setCompletionColumn(column)
-        self.pFilterModel.setFilterKeyColumn(column)
-        super(QComboSearch, self).setModelColumn(column)
+    #
+    # # on model change, update the models of the filter and completer as well
+    # def setModel(self, model):
+    #     super(QComboSearch, self).setModel(model)
+    #     self.pFilterModel.setSourceModel(model)
+    #     self.completer.setModel(self.pFilterModel)
+    #
+    # # on model column change, update the model column of the filter and completer as well
+    # def setModelColumn(self, column):
+    #     self.completer.setCompletionColumn(column)
+    #     self.pFilterModel.setFilterKeyColumn(column)
+    #     super(QComboSearch, self).setModelColumn(column)
 
     def keyPressEvent(self, event):
-        if (type(event) == QKeyEvent) and (self.textChanged == False):
-            self.setCurrentText("")
-            self.textChanged = True
+        if type(event) == QKeyEvent:
+            if self.textChanged is False:
+                self.setCurrentText("")
+                self.textChanged = True
         super(QComboSearch, self).keyPressEvent(event)
+
+    def load_data(self):
+        database = "Currencies.db"
+        conn = sqlite3.connect(database)
+        text = self.currentText()
+        phys_rows = self.select_text(conn, text, "physical")
+        dig_rows = self.select_text(conn, text, "digital")
+        self.clear()
+        self.addItems(phys_rows)
+        self.addItems(dig_rows)
+        self.setCurrentText(text)
+
+    def select_text(self, conn, s, type):
+        cur = conn.cursor()
+        cur.execute("SELECT metadata FROM " + type + "_pairs WHERE symbol like '%" + s + "%'")
+        items = cur.fetchall()
+
+        rows = []
+        for item in items:
+            rows.append(item[0])
+
+        return rows
 
 
 if __name__ == "__main__":
@@ -69,22 +101,42 @@ if __name__ == "__main__":
 
     app = QApplication(sys.argv)
 
-    with open('physical_currency_list.csv', "r") as fileInput:
-        reader = csv.reader(fileInput)
-        next(reader, None)
-        string_list = []
-        for row in reader:
-            items = [
-                str(field)
-                for field in row
-            ]
+    # cur_list = []
+    # data_list = []
+    # with open('physical_currency_list.csv', "r") as fileInput:
+    #     reader = csv.reader(fileInput)
+    #     next(reader, None)
+    #     for row in reader:
+    #         items = [
+    #             str(field)
+    #             for field in row
+    #         ]
+    #
+    #         cur_list.append(items[0])
+    #         data_list.append(items[1])
 
-            cur = items[0] + "\t" + items[1]
-            string_list.append(cur)
+    # with open('digital_currency_list.csv', "r") as fileInput:
+    #     reader = csv.reader(fileInput)
+    #     next(reader, None)
+    #     for row in reader:
+    #         items = [
+    #             str(field)
+    #             for field in row
+    #         ]
+    #
+    #         cur_list.append(items[0])
+    #         data_list.append(items[1])
+
+    # currency_pairs = []
+    # for i1, val1 in enumerate(cur_list):
+    #     for i2, val2 in enumerate(cur_list):
+    #         if val1 != val2:
+    #             currency_pairs.append(val1 + val2 +
+    #                                   "\t" + data_list[i1] + " \\ " + data_list[i2])
 
     combo = QComboSearch()
     # either fill the standard model of the combobox
-    combo.addItems(string_list)
+    #combo.addItems(currency_pairs)
     combo.setCurrentText("this is default")
 
     # or use another model
--- financial_charting.py
+++ financial_charting.py
@@ -29,11 +29,9 @@ class SymbolSearch(QWidget):
     def __init__(self):
         super().__init__()
         self.layout = QHBoxLayout()
-        self.phys_cur_list = self.make_list('physical_currency_list.csv')
 
         # search bar to find stocks, or physical/digital currency symbols
         self.combo_search = QComboSearch()
-        self.combo_search.addItems(self.phys_cur_list)
         self.combo_search.setMaximumWidth(800)
         self.combo_search.setCurrentText('Search for a symbol or currency')
         self.combo_search.setToolTip("Type here to find stock, or a physical/digital currency symbols")
@@ -45,41 +43,64 @@ class SymbolSearch(QWidget):
         self.btn_add.setToolTip("<html><head/><body><p>Chart the selected symbol, or add to the current chart</p></body></html>")
         self.btn_add.setText("Add")
 
-
         self.layout.addWidget(self.combo_search)
         self.layout.addWidget(self.btn_add)
         self.layout.setContentsMargins(0, 0, 0, 0)
+        self.setMinimumWidth(600)
         self.setLayout(self.layout)
 
-    def make_list(self, fileName):
+    # def make_currency_list(self, fileName):
         # TODO: also compile a list of all available digital currencies paired with
         #       each currency market eg BTC/CAD
+
+        # 1. make a dictionary for all single currencies, assigning the key as
+        #    an incremented number(eg 1-633). The data will consist of an
+        #    enumeration and 2 strings. The enumeration will identify what type
+        #    of currency(physical or digital) and a string for the symbol (eg CAD)
+        #    and another string for metadata (eg Canadian Dollar)
+        # 2. compile a third dictionary for every possible currency pair.
+        #    Assign each key as the concatenation of the two currencies in the pair
+        #    (eg CAD=121, USD=023, USDCAD=023121) this gives access to the dictionary
+        #    with data(enumeration, symbol, metadata)
+
 
         # 1. cylce csv list of currencies
         # 2. for each currency(first 4 chars that are not ' ' on each row)
         #    concatenate currency with every other currency excluding itself
         # 3. add each pair to a list which will be the elements of the combobox
-        with open('physical_currency_list.csv', "r") as fileInput:
-            reader = csv.reader(fileInput)
-            next(reader, None)
-            cur_list = []
-            data_list = []
-            for row in reader:
-                items = [
-                    str(field)
-                    for field in row
-                ]
+        # cur_list = []
+        # data_list = []
+        # with open('physical_currency_list.csv', "r") as fileInput:
+        #     reader = csv.reader(fileInput)
+        #     next(reader, None)
+        #     for row in reader:
+        #         items = [
+        #             str(field)
+        #             for field in row
+        #         ]
+        #
+        #         cur_list.append(items[0])
+        #         data_list.append(items[1])
+        #
+        # with open('digital_currency_list.csv', "r") as fileInput:
+        #     reader = csv.reader(fileInput)
+        #     next(reader, None)
+        #     for row in reader:
+        #         items = [
+        #             str(field)
+        #             for field in row
+        #         ]
+        #
+        #         cur_list.append(items[0])
+        #         data_list.append(items[1])
 
-                cur_list.append(items[0])
-                data_list.append(items[1])
-
-        currency_pairs = []
-        for i1, val1 in enumerate(cur_list):
-            for i2, val2 in enumerate(cur_list):
-                if val1 != val2:
-                    currency_pairs.append(val1 + val2 +
-                                          "\t" + data_list[i1] + " \\ " + data_list[i2])
-        return currency_pairs
+        # currency_pairs = []
+        # for i1, val1 in enumerate(cur_list):
+        #     for i2, val2 in enumerate(cur_list):
+        #         if val1 != val2:
+        #             currency_pairs.append(val1 + val2 +
+        #                                   "\t" + data_list[i1] + " \\ " + data_list[i2])
+        # return currency_pairs
 
 
 if __name__ == '__main__':
